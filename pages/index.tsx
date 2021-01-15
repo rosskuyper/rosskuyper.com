b4cf801916e400e083cc111caa9a1b41c19ddb6e
Ross Kuyper <ross@rosskuyper.com>
2021-01-15 18:03:09 +0200

    Foundations - TS, Prettier, Favicon

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Ross Kuyper</title>
+        <link rel="icon" href="/favicon-32x32.png" />
+      </Head>
+      <h1>Hello</h1>
+    </div>
+  )
+}
